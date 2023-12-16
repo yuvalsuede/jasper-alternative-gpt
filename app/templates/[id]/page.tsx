c6466be74e614d82fd21bf0a1542b7c6a2f51082
Deepak <pokemongamingworld123@gmail.com>
2023-12-16 12:03:02 +0545

    feat: Added dynamic page title and description for the template pages

--- app/templates/[id]/page.tsx
+++ app/templates/[id]/page.tsx
@@ -1,6 +1,7 @@
-import Layout from "../../../components/Layout";
 import { TEMPLATES } from "../../../constants/templates";
 import InputOutputComponent from "../../../components/InputOutputComponent";
+
+type Props = { params: { id: string } };
 
 export async function generateStaticParams() {
   return TEMPLATES.map((template) => ({
@@ -9,13 +10,25 @@ export async function generateStaticParams() {
     },
   }));
 }
+export async function generateMetadata({ params }: Props) {
+  const id = params.id;
+  const template = TEMPLATES.find((t) => t.id === id);
+  return { title: template?.title, description: template?.description };
+}
 
-export default function TemplatePage({ params }: { params: { id: string } }) {
+export default function TemplatePage({ params }: Props) {
   const id = params.id;
   const template = TEMPLATES.find((t) => t.id === id);
 
   if (!template) {
-    return <div>Loading...</div>;
+    return (
+      <div className=" h-full w-full grid place-items-center p-4">
+        <div className="max-w-2xl bg-slate-100 text-bold p-16 font-sans border border-black rounded-lg">
+          Not such page found
+        </div>
+      </div>
+    );
   }
+
   return <InputOutputComponent template={template} />;
 }
