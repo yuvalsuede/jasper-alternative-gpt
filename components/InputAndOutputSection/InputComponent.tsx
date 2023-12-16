537a7c202bd9d4d9c5e9fe1b2f6330ab7c059ddc
Deepak <pokemongamingworld123@gmail.com>
2023-12-16 13:58:47 +0545

    chore: added comment in InputComponent

--- components/InputAndOutputSection/InputComponent.tsx
+++ components/InputAndOutputSection/InputComponent.tsx
@@ -40,6 +40,7 @@ function InputComponent({ template, generateOutput }: InputComponentProps) {
         title={template.title}
         description={template.description}
       />
+
       <div className="flex flex-col p-4 flex-grow">
         {/* For each input displaying the correct input field which can be a 1) text input  or 2) textarea */}
         {template.inputs.map((input: TemplateInput) => (
@@ -47,6 +48,7 @@ function InputComponent({ template, generateOutput }: InputComponentProps) {
             <label htmlFor={input.id} className="block mt-4" key={input.id}>
               {input.label}
             </label>
+            {/* Displays the correct input filed */}
             <RenderCorrectInputField
               input={input}
               value={inputData[input.id] || ""}
@@ -57,6 +59,7 @@ function InputComponent({ template, generateOutput }: InputComponentProps) {
       </div>
 
       <div className="flex items-center justify-between p-4 border-t border-gray-200">
+        {/* Clears input */}
         <Button
           onClick={handleClearInputs}
           className="px-4 py-2 text-gray-600 bg-white border border-gray-200 rounded-md hover:bg-gray-100"
@@ -65,6 +68,7 @@ function InputComponent({ template, generateOutput }: InputComponentProps) {
         </Button>
 
         <Button
+          // Generating the output using ChatGPT
           onClick={async () => {
             setIsLoading(true);
             await generateOutput(template, inputData);
@@ -73,8 +77,10 @@ function InputComponent({ template, generateOutput }: InputComponentProps) {
           className={`px-4 py-2 text-white bg-gradient-to-r from-purple-400 to-blue-400 rounded-md hover:from-purple-300 hover:to-blue-300 relative ${
             isLoading ? "opacity-50" : ""
           }`}
+          // Check when the inputData is empty, or all the input fields are not filled in
           disabled={isLoading || shouldBeDisabled(inputData, template)}
         >
+          {/* The loader which is shown when the output in being generated */}
           {isLoading && (
             <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
               <div className="w-4 h-4 border-t-2 border-purple-500 border-solid rounded-full animate-spin"></div>
